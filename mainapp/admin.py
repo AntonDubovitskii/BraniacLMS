f7f253672719b9c94ee1e36380987eb34f681c02
AntonDubovitskii <gtoll@yandex.ru>
2022-12-01 00:13:39 +0400

    Add new filter

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -7,6 +7,7 @@ from mainapp import models as mainapp_models
 @admin.register(mainapp_models.News)
 class NewsAdmin(admin.ModelAdmin):
     search_fields = ["title", "preambule", "body"]
+    list_filter = ["created", "deleted"]
 
 
 @admin.register(mainapp_models.Lesson)
